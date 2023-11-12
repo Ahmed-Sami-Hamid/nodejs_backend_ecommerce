fb4af13a3ebd0ab8ad83029db6369135de53299a
ahmed sami hamid <ahmedsamihamid1@gmail.com>
2023-11-12 11:48:02 +0400

    project structure

---
+++ server.js
@@ -0,0 +1,7 @@
+import http from "http";
+import app from "./app/app.js";
+
+// create the server
+const PORT = process.env.PORT || 4000;
+const server = http.createServer(app);
+server.listen(PORT, console.log(`the server started at ${PORT}`));
